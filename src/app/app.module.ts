947a630345bbaaeeb6609987efac0f94c6e9b3c3
Sam Scott-Hake <sam@attend2it.co.uk>
2019-05-02 19:45:28 +0100

    Init commit. Working base site.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { HeaderLockupComponent } from './header-lockup/header-lockup.component';
+import { CountdownTimerComponent } from './countdown-timer/countdown-timer.component';
+import { MapComponent } from './map/map.component';
+import { SocialMediaComponent } from './social-media/social-media.component';
+import { VideosComponent } from './videos/videos.component';
+import { BlurbComponent } from './blurb/blurb.component';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
+import { LeafletModule } from '@asymmetrik/ngx-leaflet';
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderLockupComponent,
+    CountdownTimerComponent,
+    MapComponent,
+    SocialMediaComponent,
+    VideosComponent,
+    BlurbComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFontAwesomeModule,
+    LeafletModule.forRoot(),
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
